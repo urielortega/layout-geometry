094147e1a8021c52344926296c7018b5671826fc
Uriel Ortega <urielortega2509@gmail.com>
2023-07-18 12:24:52 -0600

    Finished experimental project.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                Color(
+                                    hue: min(1, geo.frame(in: .global).minY / fullView.size.height), // To use the smaller value of 1 or the calculated hue.
+                                    saturation: 1,
+                                    brightness: 1
+                                )
+                            )
+                            .rotation3DEffect(
+                                .degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5,
+                                axis: (x: 0, y: 1, z: 0)
+                            )
+                            .opacity(geo.frame(in: .global).minY / 200) // View fades out in the Y range of 0 through 200.
+                            .scaleEffect(
+                                max(
+                                    0.5, // 50% minimum size.
+                                    geo.frame(in: .global).minY / 400 // Use 1/400th of the Y position.
+                                )
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
     }
 }
 
